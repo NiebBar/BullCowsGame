919ee2e3f984077feb1b40b476fde48726588d17
NiebBar <56448752+NiebBar@users.noreply.github.com>
2019-12-15 20:00:53 +0100

    Dobrze.

--- Section_2/BullCowsGame/Main.cpp
+++ Section_2/BullCowsGame/Main.cpp
@@ -4,26 +4,36 @@
 
 using namespace std;
 
+
+void PrintIntro();
+
 int main()
 
+
 {
-	//intro
-	constexpr int WORLD_LENGT = 9;
-
-	cout << "Welcome to Bu11s and Cows";
-	cout << "Can you guess the " << WORLD_LENGT;
-	cout << " letter I'm thinking of?/n";
-	cout << endl;
+	//the entry point
+	PrintIntro();
 	
-
 
 	//get a guess from the payer
 	cout << "Enter your guess: ";
 	string guess = ""; 
-		cin >> guess;
+	getline(cin, guess);
+	
 
 	//repete the guess back to them
 	cout << "Your guess was: " << guess << endl;
+
+	//get a guess from the payer
+	cout << "Enter your guess: ";
+	string guess = "";
+	getline(cin, guess);
+
+
+	//repete the guess back to them
+	cout << "Your guess was: " << guess << endl;
+
+	
 	cout << endl;
 
 
@@ -31,5 +41,15 @@ int main()
 	return 0;
 }
 
+void PrintIntro() {
 
+	//intro
+	constexpr int WORLD_LENGT = 9;
+
+	cout << "Welcome to Bu11s and Cows";
+	cout << "Can you guess the " << WORLD_LENGT;
+	cout << " letter I'm thinking of?/n";
+	cout << endl;
+	return;
+}
 
